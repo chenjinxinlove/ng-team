88f1220b8180f1080820ed9520eaad10a24655ed
chenjinxinlove <505150704@qq.com>
2018-12-02 23:03:49 +0800

    core module share module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已存在');
+    }
+  }
+}
